9686684eb5645c2e2d9ab92177b13cf09cb66365
GeorgiPachov <georgi.patchov@gmail.com>
2014-06-19 14:16:33 +0300

    Fixed hardcoding...

--- src/FileUtils.rb
+++ src/FileUtils.rb
@@ -2,7 +2,7 @@ module FileUtils
   def self.concat_all_java_files(path, file_to_cat_in)
     open("#{path}/#{file_to_cat_in}", "w") do |big_file|
       Find.find(path) do |file|
-        is_java_file = file.end_with?(".java") && (!File.basename(file).eql? "All.java")
+        is_java_file = file.end_with?(".java") && (!File.basename(file).eql? file_to_cat_in)
         big_file << File.read(file) if is_java_file
       end
     end
